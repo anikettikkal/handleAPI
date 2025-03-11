d43c13df7fa76c051b876ddcb5af1d9c067f4889
anikettikkal <tikkalaniket2019@gmail.com>
2025-03-11 22:09:06 +0530

    frontend and backend folder created and starts code in backend

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+
+
+app.get('/api/products', (req,res) =>{
+    
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port ,() =>{
+    console.log(`server running on port ${port}`);
+    
+})
